f36011439c445ddf0a4e60d99ce943f12652f80e
dlrbgur <165352141+dlrbgur@users.noreply.github.com>
2024-04-04 18:25:31 +0900

    Add files via upload

---
+++ qwe/src/hjk.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class cvb{
+
+}
+public class hjk {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+       double a;
+        System.out.println("반지름 입력");
+       a = sc.nextInt();
+
+        System.out.println(a*2*3.14);
+    }
+}
